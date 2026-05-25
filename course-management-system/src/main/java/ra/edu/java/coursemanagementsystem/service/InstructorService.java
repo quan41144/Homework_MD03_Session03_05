e75010402908eb0efb3103332376e3ecdc6e905d
quan41144 <quan41144@gmail.com>
2026-05-25 14:41:19 +0700

    hwss0305

---
+++ course-management-system/src/main/java/ra/edu/java/coursemanagementsystem/service/InstructorService.java
@@ -0,0 +1,13 @@
+package ra.edu.java.coursemanagementsystem.service;
+
+import ra.edu.java.coursemanagementsystem.model.entity.Instructor;
+
+import java.util.List;
+
+public interface InstructorService {
+    List<Instructor> getAllInstructors();
+    Instructor getInstructorById(Long id);
+    Instructor createInstructor(Instructor instructor);
+    Instructor updateInstructor(Long id, Instructor instructor);
+    void deleteInstructorById(Long id);
+}
